ae543fc3fd76722847b2f5358585026e59e6a51e
Avellar <99830352+YagoAvellar@users.noreply.github.com>
2024-04-29 11:47:23 -0300

    Update js.js

--- js.js
+++ js.js
@@ -23,5 +23,5 @@ const server = http.createServer((req, res) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(`Servidor rodando em https://yagoavellar.github.io/:${PORT}`);
 });
